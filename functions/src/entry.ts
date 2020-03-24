4f0f4f0bdbe4a975d9d24769bdc7ff6893431143
Aki.SHIMO <fezrestia@gmail.com>
2020-03-24 19:59:32 +0900

    ADD: Puppeteer scraping first sample

--- functions/src/entry.ts
+++ functions/src/entry.ts
@@ -1,4 +1,7 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+
+const puppeteer = require("puppeteer");
+import { ElementHandle } from "puppeteer";
 
 const runtimeConfig: { timeoutSeconds: number, memory: "128MB"|"256MB"|"512MB"|"1GB"|"2GB" } = {
   timeoutSeconds: 300,
@@ -12,3 +15,40 @@ export const checkStatus = functions
   response.send("OK");
 });
 
+
+
+//// DCM SIM STATS ///////////////////////////////////////////////////////////////////////////////
+//
+//
+
+const DCM_TOP_URL = "https://www.nttdocomo.co.jp/mydocomo/data";
+
+export const updateDcmStats = functions
+    .runWith(runtimeConfig)
+    .https
+    .onRequest( async (request, response) => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      // Top page.
+      await page.goto(DCM_TOP_URL);
+
+      // Search login URL.
+      let links: ElementHandle[] = await page.$$("a");
+      let urls: string[] = [];
+      links.forEach( async (link: ElementHandle) => {
+        let prop = await link.getProperty("href");
+        let json = await prop.jsonValue();
+        urls.push(json.toString());
+      } );
+
+
+
+
+
+      await browser.close();
+
+      response.send(urls.join(", "));
+    } );
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
